0d6f4d8585241dfc7c73655be528b9f6ed233e38
sanjay kumawat <skumawat@visitly.io>
2026-02-11 14:53:18 +0530

    removed the unused code

--- apps/host-mfe/src/routes/index.tsx
+++ apps/host-mfe/src/routes/index.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import {BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 
 function AppRouter() {
   return (
-    //  <BrowserRouter>
       <Routes>  
-      
         <Route path="/" element={<Navigate to="login" replace />} />
-
       </Routes>
-      // </BrowserRouter>
    
   );
 }
