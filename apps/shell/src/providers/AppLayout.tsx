baad0aa81a1aa089fc808b8397e4116cb139dc01
sanjay kumawat <skumawat@visitly.io>
2026-02-11 16:15:02 +0530

    app layout

--- apps/shell/src/providers/AppLayout.tsx
+++ apps/shell/src/providers/AppLayout.tsx
@@ -1,24 +1,54 @@
-import { useAuthStore } from "@visitly/app-store";
-import { Outlet } from "react-router-dom";
 
-function AppLayout() {
-    // const auth = useAuthStore();
-  
-    // if (auth.status !== 'authenticated') {
-    //   return <Outlet />; // login / public pages
-    // }
-  
+https://3vza0x99ll.execute-api.us-west-2.amazonaws.com/development/v1/visit/preregister
+
+
+https://3vza0x99ll.execute-api.us-west-2.amazonaws.com/development/v1/visit/preregister
+
+
+
+
+
+Request URL
+https://3vza0x99ll.execute-api.us-west-2.amazonaws.com/development/v1/visit/preregister/b5bb5c44-8ae0-48c0-910f-109891d59b1f?updateType=SELECTED_VISIT
+
+
+sanjayK@12345
+
+skumawat@visitly.io
+sanjayK@12345
+
+created sidebar and header including location select (some css issue needs to be fix)
+write custom event for navigation between react and angular.
+(remove sidebar and header from angular)
+
+sync/feat
+
+
+
+
+import { Outlet } from 'react-router-dom';
+import { AppSidebar, AppHeader, useSidebarStore } from '@/shared/components';
+import { useState } from 'react';
+
+const AppLayout = () => {
+    const {isCollapsed, setCollapsed} = useSidebarStore();
     return (
-      <div className="app-shell">
-        {/* <Header /> */}
-        <div className="body">
-          {/* <Sidebar /> */}
-          <main>
-            <Outlet />
-          </main>
+        <div id="app-layout"  className='tw:bg-[#E5E9FF]'>
+            {/* Sidebar */}
+            <AppHeader onToggle={() => setCollapsed(!isCollapsed)}  isCollapsed={isCollapsed}/>
+            
+            {/* Main content area */}
+            <div className="tw:flex ">
+                {/* Header */}
+                <AppSidebar  />
+                {/* Routed content */}
+                <main className="tw:flex-1 tw:overflow-auto">
+                    <Outlet />
+                </main>
+
+            </div>
         </div>
-      </div>
     );
-  }
-  
-  export default AppLayout;
+};
+
+export default AppLayout;
